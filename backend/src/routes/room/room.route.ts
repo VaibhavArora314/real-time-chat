5c0ee753061cd69f2e0b514f97c9d57ef4347ecb
VaibhavArora314 <vaibhav.afw@gmail.com>
2024-03-11 14:16:39 +0530

    verify invite code endpoint

--- backend/src/routes/room/room.route.ts
+++ backend/src/routes/room/room.route.ts
@@ -1,90 +1,96 @@
 import { Router, Response } from "express";
 import authMiddleware from "../../middlewares/authMiddleware";
-import { UserAuthRequest } from "../../helpers/types";
+import { RoomInteface, UserAuthRequest } from "../../helpers/types";
 import { Room, User } from "../../db";
 import STATUS_CODES from "../../helpers/statusCodes";
 import mongoose from "mongoose";
+import { formatRoom } from "../../helpers";
 
 const roomRouter = Router();
 
-interface ErrorSchema {
-    title?: string,
-    description?: string,
-    other?: string
-}
-
-roomRouter.get('/my-rooms', authMiddleware, async (req: UserAuthRequest, res: Response) => {
+roomRouter.get(
+  "/my-rooms",
+  authMiddleware,
+  async (req: UserAuthRequest, res: Response) => {
     const userId = req.userId;
 
     const user = await User.findOne({ _id: userId }).populate("rooms");
 
     return res.status(STATUS_CODES.OK).json({
-        rooms: user?.rooms ? user.rooms.map((room: any) => ({
+      rooms: user?.rooms
+        ? user.rooms.map((room: any) => ({
             _id: room._id,
             title: room?.title,
             lastActivity: room?.lastActivity,
             lastMessage: room?.lastMessage,
-        })) : [],
-    })
-})
+          }))
+        : [],
+    });
+  }
+);
 
-roomRouter.get('/my-rooms/:id', authMiddleware, async (req: UserAuthRequest, res: Response) => {
+roomRouter.get(
+  "/my-rooms/:id",
+  authMiddleware,
+  async (req: UserAuthRequest, res: Response) => {
     const userId = req.userId;
     const roomId = req.params.id;
 
     if (!mongoose.Types.ObjectId.isValid(roomId)) {
-        return res.status(STATUS_CODES.BadRequest).json({
-            error: "Invalid roomId!",
-        })
+      return res.status(STATUS_CODES.BadRequest).json({
+        error: "Invalid roomId!",
+      });
     }
 
-    const user = await User.findOne({ _id: userId, rooms: {
-        "$in" : roomId
-    } });
+    const user = await User.findOne({
+      _id: userId,
+      rooms: {
+        $in: roomId,
+      },
+    });
 
     if (!user) {
+      return res.status(STATUS_CODES.BadRequest).json({
+        error:
+          "This room may not exist or your do not have access to view this!",
+      });
+    }
+
+    const room: RoomInteface | null = await Room.findOne({ _id: roomId })
+      .populate("admin")
+      .populate("participants")
+      .populate({
+        path: "messages",
+        populate: {
+          path: "sender",
+        },
+      });
+
+    return res.status(STATUS_CODES.OK).json({
+      room: room ? formatRoom(room, user._id.toString()) : {},
+    });
+  }
+);
+
+roomRouter.post('/verify-invite', authMiddleware, async (req:UserAuthRequest, res: Response) => {
+    const {inviteCode} = req.body;
+
+    if (!inviteCode || typeof inviteCode != "string") {
         return res.status(STATUS_CODES.BadRequest).json({
-            error: "This room may not exist or your do not have access to view this!",
+            error: "Invalid invite code!",
         })
     }
 
-    const room:any = await Room.findOne({_id: roomId}).populate("admin").populate("participants").populate({
-        path: "messages",
-        populate: {
-            path: "sender"
-        }
-    })
+    const room = await Room.findOne({inviteCode:inviteCode});
+
+    if (!room) {
+        return res.status(STATUS_CODES.BadRequest).json({
+            error: "Invalid invite code!",
+        })
+    }
 
     return res.status(STATUS_CODES.OK).json({
-        room : {
-            _id: room?._id,
-            title: room?.title,
-            description: room?.description,
-            admin: {
-                _id: room?.admin?._id,
-                username: room?.admin?.username,
-                email: room?.admin?.email
-            },
-            inviteCode: (userId == room?.admin?._id) ? room?.inviteCode : null,
-            participants: room?.participants?.map((user: any) => ({
-                _id: user?._id,
-                username: user?.username,
-                email: user?.email
-            })),
-            messages: room?.messages?.map((message: any) => ({
-                _id: message?._id,
-                sender: {
-                    _id: message?.sender?._id,
-                    username: message?.sender?.username,
-                    email: message?.sender?.email,
-                },
-                content: message?.content,
-                creationDate: message?.creationDate,
-            })),
-            creationDate: room?.creationDate,
-            lastActivity: room?.lastActivity,
-            lastMessage: room?.lastMessage,
-        },
+        message: "Invite code is valid!"
     })
 })
 
--- frontend/src/components/JoinRoomModal.tsx
+++ frontend/src/components/JoinRoomModal.tsx
@@ -1,24 +1,51 @@
-import { useState } from "react";
+import React ,{ useState } from "react";
 import Input from "./Input";
 import Button from "./Button";
+import axios, { AxiosError } from "axios";
+import { useRecoilValue } from "recoil";
+import { tokenState } from "../store/atoms/auth";
 
 interface JoinRoomModalProps {
   handleJoinModalClose: () => void;
   joinRoom: (roomCode:string) => void
 }
 
+interface ResponseData {
+  message?: string,
+  error?: string,
+}
+
 const JoinRoomModal = ({ handleJoinModalClose, joinRoom }: JoinRoomModalProps) => {
   const [roomCode, setRoomCode] = useState<string>("");
+  const [error,setError] = useState<string>("");
+  const token = useRecoilValue(tokenState);
+
+  const handleRoomJoin = async (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      await axios.post<ResponseData>('/api/v1/room/verify-invite', {
+        inviteCode: roomCode
+      }, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+
+      joinRoom(roomCode);
+      handleJoinModalClose();
+    } catch (error) {
+      const axiosError = error as AxiosError<ResponseData>;
+      const data = axiosError.response?.data;
+      setError(data?.error ? data.error : "An unexpected error occurred");
+    }
+  }
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-900 bg-opacity-50">
       <div className="bg-white p-8 rounded-lg w-80">
         <h2 className="text-xl font-semibold mb-4 text-center">Join room</h2>
-        <form className="space-y-4" onSubmit={(e) => {
-          e.preventDefault();
-
-          joinRoom(roomCode);
-        }}>
+        <form className="space-y-4" onSubmit={handleRoomJoin}>
           <Input
             id="code"
             label="Enter group invite code"
@@ -27,7 +54,7 @@ const JoinRoomModal = ({ handleJoinModalClose, joinRoom }: JoinRoomModalProps) =
             handleOnChange={({ value }) => {
               setRoomCode(value);
             }}
-            error=""
+            error={error}
             required
           />
           <div className="flex flex-row justify-between">
